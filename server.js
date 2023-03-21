6c20be6daf6aaddf2ea5d3c110820c13316369df
maryamh12 <maryam_hazrati94@yahoo.com>
2023-03-21 17:23:12 +0000

    third

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/errorHandler.js";
 import router from "./router.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 const PORT = 999;
 
 // logger
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 app.use(router);
