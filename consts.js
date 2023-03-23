bfff5cd2999ad3e063bb08cc8184b0e79670d6fd
Phoenix Duong <phoenixduong@hotmail.co.uk>
2023-03-23 21:21:07 +0000

    added the mongodb cloud connection

--- consts.js
+++ consts.js
@@ -2,7 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const DB_CONNECTION_STRING =
+//   process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/drinks";
+
 export const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/drinks";
+  process.env.DB_CONNECTION_STRING ||
+  "mongodb+srv://admin:AdminTest@cluster0.jjwlwrq.mongodb.net/test";
 
 export const JWT_SECRET = process.env.JWT_SECRET || "cola";
